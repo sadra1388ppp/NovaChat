d9ac2df3080c3dd624375eff4196f0878b6393ce
sadra <sadrap31@gmail.com>
2026-08-28 21:31:54 +0330

    Stage 11: open Contacts from Profile

--- NovaChat.Client/MainWindow.xaml.cs
+++ NovaChat.Client/MainWindow.xaml.cs
@@ -10,98 +10,84 @@ namespace NovaChat.Client
         public MainWindow()
         {
             InitializeComponent();
-
             LoadLightTheme();
-
             ShowLogin();
         }
 
         public void ShowLogin()
         {
             _isOwner = false;
-
             MainContainer.Children.Clear();
 
             LoginView loginView = new LoginView();
-
             loginView.CreateAccountRequested += ShowRegister;
             loginView.LoginSuccessful += HandleNormalUserLogin;
             loginView.OwnerLoginSuccessful += HandleOwnerLogin;
-
             MainContainer.Children.Add(loginView);
         }
 
         private void HandleNormalUserLogin()
         {
             _isOwner = false;
-
             ShowMain();
         }
 
         private void HandleOwnerLogin()
         {
             _isOwner = true;
-
             ShowMain();
         }
 
         public void ShowRegister()
         {
             MainContainer.Children.Clear();
-
             RegisterView registerView = new RegisterView();
-
             registerView.BackToLoginRequested += ShowLogin;
-
             MainContainer.Children.Add(registerView);
         }
 
         public void ShowMain()
         {
             MainContainer.Children.Clear();
-
             MainView mainView = new MainView();
-
             mainView.ProfileRequested += ShowProfile;
             mainView.SettingsRequested += ShowSettings;
-
             mainView.SetOwnerMode(_isOwner);
-
             MainContainer.Children.Add(mainView);
         }
 
         public void ShowProfile()
         {
             MainContainer.Children.Clear();
-
             ProfileView profileView = new ProfileView();
-
             profileView.BackToChatRequested += ShowMain;
-
+            profileView.ContactsRequested += ShowContacts;
             MainContainer.Children.Add(profileView);
+        }
+
+        public void ShowContacts()
+        {
+            MainContainer.Children.Clear();
+            ContactsView contactsView = new ContactsView();
+            contactsView.BackToChatRequested += ShowMain;
+            MainContainer.Children.Add(contactsView);
         }
 
         public void ShowSettings()
         {
             MainContainer.Children.Clear();
-
             SettingsView settingsView = new SettingsView();
-
             settingsView.BackToChatRequested += ShowMain;
-
             MainContainer.Children.Add(settingsView);
         }
 
         private void LoadLightTheme()
         {
             Application.Current.Resources.MergedDictionaries.Clear();
-
             Application.Current.Resources.MergedDictionaries.Add(
                 new ResourceDictionary
                 {
-                    Source = new System.Uri(
-                        "Resources/LightTheme.xaml",
-                        System.UriKind.Relative)
+                    Source = new System.Uri("Resources/LightTheme.xaml", System.UriKind.Relative)
                 });
         }
     }
