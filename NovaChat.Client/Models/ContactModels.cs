c52cd17a12fb96953363f5555c7db90afc1e99fb
sadra <sadrap31@gmail.com>
2026-08-28 21:31:25 +0330

    Stage 11: add client contact models

---
+++ NovaChat.Client/Models/ContactModels.cs
@@ -0,0 +1,14 @@
+namespace NovaChat.Client.Models;
+
+public class ContactModel
+{
+    public string UserId { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime AddedAt { get; set; }
+}
+
+public class AddContactRequest
+{
+    public string UserId { get; set; } = string.Empty;
+}
