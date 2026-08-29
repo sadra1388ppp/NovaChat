1926a82c568f9293b4cf57e11bb92b2a9e9c7288
sadra <sadrap31@gmail.com>
2026-08-29 07:56:19 +0330

    Add private message deletion state

--- NovaChat.Server/Entities/Message.cs
+++ NovaChat.Server/Entities/Message.cs
@@ -3,14 +3,12 @@ namespace NovaChat.Server.Entities;
 public class Message
 {
     public int Id { get; set; }
-
     public int ChatId { get; set; }
     public Chat Chat { get; set; } = null!;
-
     public string SenderId { get; set; } = string.Empty;
     public User Sender { get; set; } = null!;
-
     public string Content { get; set; } = string.Empty;
-
     public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    public bool DeletedForEveryone { get; set; }
+    public string DeletedForUserIds { get; set; } = string.Empty;
 }
