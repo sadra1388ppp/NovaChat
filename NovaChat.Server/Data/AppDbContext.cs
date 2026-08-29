985335ac223509157a192c7af53ca4b5dc7aa1c2
sadra <sadrap31@gmail.com>
2026-08-29 07:14:33 +0330

    feat(groups): configure group database model

--- NovaChat.Server/Data/AppDbContext.cs
+++ NovaChat.Server/Data/AppDbContext.cs
@@ -5,61 +5,34 @@ namespace NovaChat.Server.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; } = null!;
-
     public DbSet<Chat> Chats { get; set; } = null!;
-
     public DbSet<Message> Messages { get; set; } = null!;
-
     public DbSet<Contact> Contacts { get; set; } = null!;
+    public DbSet<Group> Groups { get; set; } = null!;
+    public DbSet<GroupMember> GroupMembers { get; set; } = null!;
+    public DbSet<GroupMessage> GroupMessages { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Chat>()
-            .HasOne(c => c.User1)
-            .WithMany()
-            .HasForeignKey(c => c.User1Id)
-            .OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Chat>().HasOne(c => c.User1).WithMany().HasForeignKey(c => c.User1Id).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Chat>().HasOne(c => c.User2).WithMany().HasForeignKey(c => c.User2Id).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Message>().HasOne(m => m.Chat).WithMany(c => c.Messages).HasForeignKey(m => m.ChatId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<Message>().HasOne(m => m.Sender).WithMany().HasForeignKey(m => m.SenderId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Contact>().HasOne(c => c.OwnerUser).WithMany().HasForeignKey(c => c.OwnerUserId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<Contact>().HasOne(c => c.ContactUser).WithMany().HasForeignKey(c => c.ContactUserId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<Contact>().HasIndex(c => new { c.OwnerUserId, c.ContactUserId }).IsUnique();
 
-        modelBuilder.Entity<Chat>()
-            .HasOne(c => c.User2)
-            .WithMany()
-            .HasForeignKey(c => c.User2Id)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Message>()
-            .HasOne(m => m.Chat)
-            .WithMany(c => c.Messages)
-            .HasForeignKey(m => m.ChatId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<Message>()
-            .HasOne(m => m.Sender)
-            .WithMany()
-            .HasForeignKey(m => m.SenderId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Contact>()
-            .HasOne(c => c.OwnerUser)
-            .WithMany()
-            .HasForeignKey(c => c.OwnerUserId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<Contact>()
-            .HasOne(c => c.ContactUser)
-            .WithMany()
-            .HasForeignKey(c => c.ContactUserId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Contact>()
-            .HasIndex(c => new { c.OwnerUserId, c.ContactUserId })
-            .IsUnique();
+        modelBuilder.Entity<Group>().HasOne(g => g.Creator).WithMany().HasForeignKey(g => g.CreatorId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<GroupMember>().HasKey(m => new { m.GroupId, m.UserId });
+        modelBuilder.Entity<GroupMember>().HasOne(m => m.Group).WithMany(g => g.Members).HasForeignKey(m => m.GroupId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<GroupMember>().HasOne(m => m.User).WithMany().HasForeignKey(m => m.UserId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<GroupMessage>().HasOne(m => m.Group).WithMany(g => g.Messages).HasForeignKey(m => m.GroupId).OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<GroupMessage>().HasOne(m => m.Sender).WithMany().HasForeignKey(m => m.SenderId).OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.Entity<GroupMember>().Property(m => m.Role).HasConversion<string>();
     }
 }
