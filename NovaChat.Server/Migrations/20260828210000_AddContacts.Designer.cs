23cea48a27d8f548821217410d082ab6bebe7131
sadra <sadrap31@gmail.com>
2026-08-30 12:03:17 +0330

    Fix EF migration discovery for AddContacts

---
+++ NovaChat.Server/Migrations/20260828210000_AddContacts.Designer.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+#nullable disable
+
+namespace NovaChat.Server.Migrations;
+
+[DbContext(typeof(Data.AppDbContext))]
+[Migration("20260828210000_AddContacts")]
+partial class AddContacts
+{
+}
