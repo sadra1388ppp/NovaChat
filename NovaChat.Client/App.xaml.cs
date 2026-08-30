9f54626641db3ea68de9b7d1d08681c196dfe1a8
sadra <sadrap31@gmail.com>
2026-08-30 17:46:04 +0330

    Register NovaChat media UI handlers at application startup

--- NovaChat.Client/App.xaml.cs
+++ NovaChat.Client/App.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using NovaChat.Client.Views;
 
 namespace NovaChat.Client;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainView.RegisterMediaFeatures();
+        base.OnStartup(e);
+    }
 }
-
