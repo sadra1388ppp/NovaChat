222152b73c03e4808f90e2accc33a1983deec6ed
sadra <sadrap31@gmail.com>
2026-08-30 17:27:28 +0330

    Fix ProfileView join-date partial lifecycle hook

--- NovaChat.Client/Views/ProfileView.JoinDateEnglish.cs
+++ NovaChat.Client/Views/ProfileView.JoinDateEnglish.cs
@@ -1,23 +1,7 @@
-using System.Globalization;
-using System.Windows;
-
 namespace NovaChat.Client.Views;
 
+// Join date formatting is handled directly by ProfileView.UpdateProfileTextUi.
+// This partial intentionally contains no event registration so it cannot interfere with ProfileView lifecycle events.
 public partial class ProfileView
 {
-    static ProfileView()
-    {
-        EventManager.RegisterClassHandler(
-            typeof(ProfileView),
-            FrameworkElement.DataContextChangedEvent,
-            new DependencyPropertyChangedEventHandler(OnProfileDataContextChanged));
-    }
-
-    private static void OnProfileDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
-    {
-        if (sender is not ProfileView view || view.JoinedText == null || view.DataContext is not Models.ProfileModel profile)
-            return;
-
-        view.JoinedText.Text = $"Joined {profile.CreatedAt.ToLocalTime().ToString("dd MMM yyyy", CultureInfo.InvariantCulture)}";
-    }
 }
