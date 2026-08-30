60741a77106a0777f7a784d9bbfc0c5d08d71c76
sadra <sadrap31@gmail.com>
2026-08-30 12:03:24 +0330

    Fix EF migration discovery for profile repair

---
+++ NovaChat.Server/Migrations/20260830123000_RepairUserProfileColumns.Designer.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+#nullable disable
+
+namespace NovaChat.Server.Migrations;
+
+[DbContext(typeof(Data.AppDbContext))]
+[Migration("20260830123000_RepairUserProfileColumns")]
+partial class RepairUserProfileColumns
+{
+}
