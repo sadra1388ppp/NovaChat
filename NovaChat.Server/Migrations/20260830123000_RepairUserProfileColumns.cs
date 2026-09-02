50d29e81e98648f2e251dea795139f169ebedc4e
sadra <sadrap31@gmail.com>
2026-09-03 03:13:03 +0330

    Disable legacy PostgreSQL repair migration

--- NovaChat.Server/Migrations/20260830123000_RepairUserProfileColumns.cs
+++ NovaChat.Server/Migrations/20260830123000_RepairUserProfileColumns.cs
@@ -1,22 +1,2 @@
-using Microsoft.EntityFrameworkCore.Migrations;
-
-#nullable disable
-
-namespace NovaChat.Server.Migrations;
-
-public partial class RepairUserProfileColumns : Migration
-{
-    protected override void Up(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.Sql("ALTER TABLE \"Users\" ADD COLUMN IF NOT EXISTS \"Bio\" text NOT NULL DEFAULT '';");
-        migrationBuilder.Sql("ALTER TABLE \"Users\" ADD COLUMN IF NOT EXISTS \"AvatarUrl\" text NULL;");
-        migrationBuilder.Sql("ALTER TABLE \"Users\" ADD COLUMN IF NOT EXISTS \"LastSeenAt\" timestamp with time zone NULL;");
-    }
-
-    protected override void Down(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.Sql("ALTER TABLE \"Users\" DROP COLUMN IF EXISTS \"LastSeenAt\";");
-        migrationBuilder.Sql("ALTER TABLE \"Users\" DROP COLUMN IF EXISTS \"AvatarUrl\";");
-        migrationBuilder.Sql("ALTER TABLE \"Users\" DROP COLUMN IF EXISTS \"Bio\";");
-    }
-}
+// Legacy PostgreSQL migration removed for the MariaDB migration reset.
+// Generate a new migration with EF Core 9 + Pomelo after pulling the MariaDB version.
