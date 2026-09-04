d9cd1865198c4369223644ae7ef73c2f73356e1e
sadra <sadrap31@gmail.com>
2026-09-04 14:03:52 +0330

    Remove Server Overview handler

--- NovaChat.Client/Views/MainView.OwnerHandlers.cs
+++ NovaChat.Client/Views/MainView.OwnerHandlers.cs
@@ -11,9 +11,6 @@ public partial class MainView
         (Window.GetWindow(this) as MainWindow)?.ShowManageUsers();
     }
 
-    private void ServerOverviewButton_Click(object sender, RoutedEventArgs e)
-        => MessageBox.Show("Server overview is coming next.", "NovaChat Owner", MessageBoxButton.OK, MessageBoxImage.Information);
-
     private void AdminSettingsButton_Click(object sender, RoutedEventArgs e)
         => MessageBox.Show("Admin settings are coming next.", "NovaChat Owner", MessageBoxButton.OK, MessageBoxImage.Information);
 }
