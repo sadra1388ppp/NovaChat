f7b4786b919859f412b022ef585a88e77dfe0c7d
sadra <sadrap31@gmail.com>
2026-09-06 01:49:05 +0330

    Update admin user lookup for numeric IDs

--- NovaChat.Server/Services/AdminService.cs
+++ NovaChat.Server/Services/AdminService.cs
@@ -6,19 +6,7 @@ namespace NovaChat.Server.Services;
 public class AdminService
 {
     private readonly AppDbContext _db;
-
-    public AdminService(AppDbContext db)
-    {
-        _db = db;
-    }
-
-    public async Task<int> GetUserCountAsync()
-    {
-        return await _db.Users.CountAsync();
-    }
-
-    public async Task<bool> UserExistsAsync(string id)
-    {
-        return await _db.Users.AnyAsync(u => u.Id == id);
-    }
+    public AdminService(AppDbContext db) => _db = db;
+    public Task<int> GetUserCountAsync() => _db.Users.CountAsync();
+    public Task<bool> UserExistsAsync(string id) => long.TryParse(id, out var userId) && userId > 0 ? _db.Users.AnyAsync(u => u.Id == userId) : Task.FromResult(false);
 }
