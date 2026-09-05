ff73906479682ca162fe52640534eec8b9bbe29e
sadra <sadrap31@gmail.com>
2026-09-06 01:48:04 +0330

    Use numeric internal contact foreign keys

--- NovaChat.Server/Entities/Contact.cs
+++ NovaChat.Server/Entities/Contact.cs
@@ -3,14 +3,9 @@ namespace NovaChat.Server.Entities;
 public class Contact
 {
     public int Id { get; set; }
-
-    public string OwnerUserId { get; set; } = string.Empty;
-
-    public string ContactUserId { get; set; } = string.Empty;
-
+    public long OwnerUserId { get; set; }
+    public long ContactUserId { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
     public User OwnerUser { get; set; } = null!;
-
     public User ContactUser { get; set; } = null!;
 }
