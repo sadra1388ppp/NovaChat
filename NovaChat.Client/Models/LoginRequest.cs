8316ebcab755be87d2dd2d7e14670144e5bf6e86
sadra <sadrap31@gmail.com>
2026-09-06 00:55:25 +0330

    Keep existing login UI code compatible while sending Login

--- NovaChat.Client/Models/LoginRequest.cs
+++ NovaChat.Client/Models/LoginRequest.cs
@@ -1,8 +1,18 @@
+using System.Text.Json.Serialization;
+
 namespace NovaChat.Client.Models;
 
 public class LoginRequest
 {
     public string Login { get; set; } = string.Empty;
+
+    // Backward-compatible alias for the existing client code. It is not sent over the wire.
+    [JsonIgnore]
+    public string Id
+    {
+        get => Login;
+        set => Login = value;
+    }
 
     public string Password { get; set; } = string.Empty;
 }
