dc2f0b439352193799038b47737282dc686cb90f
sadra <sadrap31@gmail.com>
2026-09-06 00:55:18 +0330

    Use Username or phone number for login

--- NovaChat.Client/Models/LoginRequest.cs
+++ NovaChat.Client/Models/LoginRequest.cs
@@ -2,7 +2,7 @@ namespace NovaChat.Client.Models;
 
 public class LoginRequest
 {
-    public string Id { get; set; } = string.Empty;
+    public string Login { get; set; } = string.Empty;
 
     public string Password { get; set; } = string.Empty;
 }
