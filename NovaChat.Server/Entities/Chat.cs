a6d22bfbc486041f10899e0d9739131ff80e495d
sadra <sadrap31@gmail.com>
2026-09-06 01:47:58 +0330

    Use numeric internal user foreign keys in chats

--- NovaChat.Server/Entities/Chat.cs
+++ NovaChat.Server/Entities/Chat.cs
@@ -3,14 +3,10 @@ namespace NovaChat.Server.Entities;
 public class Chat
 {
     public int Id { get; set; }
-
-    public string User1Id { get; set; } = string.Empty;
+    public long User1Id { get; set; }
     public User User1 { get; set; } = null!;
-
-    public string User2Id { get; set; } = string.Empty;
+    public long User2Id { get; set; }
     public User User2 { get; set; } = null!;
-
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
     public ICollection<Message> Messages { get; set; } = new List<Message>();
 }
