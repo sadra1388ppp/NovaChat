67cfc232170d794ef6b6190d00472f21c12ea89e
sadra <sadrap31@gmail.com>
2026-09-06 00:55:20 +0330

    Use Username instead of User Id during registration

--- NovaChat.Client/Models/RegisterRequest.cs
+++ NovaChat.Client/Models/RegisterRequest.cs
@@ -2,7 +2,7 @@ namespace NovaChat.Client.Models;
 
 public class RegisterRequest
 {
-    public string Id { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
 
     public string DisplayName { get; set; } = string.Empty;
 
