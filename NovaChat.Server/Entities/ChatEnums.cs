f3bdae4e766511eed5661cf3adfe3203119ef3a6
sadra <sadrap31@gmail.com>
2026-09-09 16:45:23 +0330

    Remove duplicate ChatType declaration

--- NovaChat.Server/Entities/ChatEnums.cs
+++ NovaChat.Server/Entities/ChatEnums.cs
@@ -1,13 +1,5 @@
 namespace NovaChat.Server.Entities;
 
-// Domain enums stay outside generated files. MariaDB INT columns scaffold as int;
-// use integer enum constants in LINQ and cast back when creating API responses.
-public enum ChatType
-{
-    Private = 0,
-    Group = 1
-}
-
 public enum ChatMemberRole
 {
     Member = 0,
