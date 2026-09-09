1d1bc3a461f0aa2b2fb448ab372e3fde383aa31a
sadra <sadrap31@gmail.com>
2026-09-09 16:59:15 +0330

    Restore Chat compatibility projections

--- NovaChat.Server/Entities/Chat.cs
+++ NovaChat.Server/Entities/Chat.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace NovaChat.Server.Entities;
 
 public enum ChatType
@@ -25,4 +27,21 @@ public partial class Chat
     public virtual ICollection<ChatMember> ChatMembers { get; set; } = new List<ChatMember>();
 
     public virtual ICollection<Message> Messages { get; set; } = new List<Message>();
+
+    // Compatibility projections for existing private-chat UI/API code.
+    // Private chats are actually persisted through ChatMembers.
+    [NotMapped]
+    public long? User1Id { get; set; }
+
+    [NotMapped]
+    public long? User2Id { get; set; }
+
+    [NotMapped]
+    public User? User1 { get; set; }
+
+    [NotMapped]
+    public User? User2 { get; set; }
+
+    [NotMapped]
+    public ICollection<ChatMember> Members => ChatMembers;
 }
