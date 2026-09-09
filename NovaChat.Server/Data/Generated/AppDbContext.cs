63ed595e49fd02ba92a428ec3608516b18b73215
sadra <sadrap31@gmail.com>
2026-09-09 14:05:58 +0330

    Remove Chat user slot mappings

--- NovaChat.Server/Data/Generated/AppDbContext.cs
+++ NovaChat.Server/Data/Generated/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using NovaChat.Server.Entities;
@@ -13,13 +13,9 @@ public partial class AppDbContext : DbContext
     }
 
     public virtual DbSet<Chat> Chats { get; set; }
-
     public virtual DbSet<ChatMember> ChatMembers { get; set; }
-
     public virtual DbSet<Contact> Contacts { get; set; }
-
     public virtual DbSet<Message> Messages { get; set; }
-
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -31,14 +27,7 @@ public partial class AppDbContext : DbContext
         modelBuilder.Entity<Chat>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.HasIndex(e => e.CreatedByUserId, "IX_Chats_CreatedByUserId");
-
-            entity.HasIndex(e => new { e.Type, e.User1Id, e.User2Id }, "IX_Chats_Type_User1Id_User2Id");
-
-            entity.HasIndex(e => e.User1Id, "IX_Chats_User1Id");
-
-            entity.HasIndex(e => e.User2Id, "IX_Chats_User2Id");
 
             entity.Property(e => e.Id).HasColumnType("int(11)");
             entity.Property(e => e.AvatarUrl).HasMaxLength(512);
@@ -46,26 +35,16 @@ public partial class AppDbContext : DbContext
             entity.Property(e => e.CreatedByUserId).HasColumnType("bigint(20)");
             entity.Property(e => e.Name).HasMaxLength(128);
             entity.Property(e => e.Type).HasColumnType("int(11)");
-            entity.Property(e => e.User1Id).HasColumnType("bigint(20)");
-            entity.Property(e => e.User2Id).HasColumnType("bigint(20)");
 
-            entity.HasOne(d => d.CreatedByUser).WithMany(p => p.ChatCreatedByUsers).HasForeignKey(d => d.CreatedByUserId);
-
-            entity.HasOne(d => d.User1).WithMany(p => p.ChatUser1s)
-                .HasForeignKey(d => d.User1Id)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            entity.HasOne(d => d.User2).WithMany(p => p.ChatUser2s)
-                .HasForeignKey(d => d.User2Id)
-                .OnDelete(DeleteBehavior.Cascade);
+            entity.HasOne(d => d.CreatedByUser)
+                .WithMany(p => p.ChatCreatedByUsers)
+                .HasForeignKey(d => d.CreatedByUserId);
         });
 
         modelBuilder.Entity<ChatMember>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.HasIndex(e => new { e.ChatId, e.UserId }, "IX_ChatMembers_ChatId_UserId").IsUnique();
-
             entity.HasIndex(e => e.UserId, "IX_ChatMembers_UserId");
 
             entity.Property(e => e.Id).HasColumnType("int(11)");
@@ -75,16 +54,13 @@ public partial class AppDbContext : DbContext
             entity.Property(e => e.UserId).HasColumnType("bigint(20)");
 
             entity.HasOne(d => d.Chat).WithMany(p => p.ChatMembers).HasForeignKey(d => d.ChatId);
-
             entity.HasOne(d => d.User).WithMany(p => p.ChatMembers).HasForeignKey(d => d.UserId);
         });
 
         modelBuilder.Entity<Contact>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.HasIndex(e => e.ContactUserId, "IX_Contacts_ContactUserId");
-
             entity.HasIndex(e => new { e.OwnerUserId, e.ContactUserId }, "IX_Contacts_OwnerUserId_ContactUserId").IsUnique();
 
             entity.Property(e => e.Id).HasColumnType("int(11)");
@@ -93,16 +69,13 @@ public partial class AppDbContext : DbContext
             entity.Property(e => e.OwnerUserId).HasColumnType("bigint(20)");
 
             entity.HasOne(d => d.ContactUser).WithMany(p => p.ContactContactUsers).HasForeignKey(d => d.ContactUserId);
-
             entity.HasOne(d => d.OwnerUser).WithMany(p => p.ContactOwnerUsers).HasForeignKey(d => d.OwnerUserId);
         });
 
         modelBuilder.Entity<Message>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.HasIndex(e => new { e.ChatId, e.SentAt, e.Id }, "IX_Messages_ChatId_SentAt_Id");
-
             entity.HasIndex(e => e.SenderId, "IX_Messages_SenderId");
 
             entity.Property(e => e.Id).HasColumnType("int(11)");
@@ -111,18 +84,14 @@ public partial class AppDbContext : DbContext
             entity.Property(e => e.SentAt).HasMaxLength(6);
 
             entity.HasOne(d => d.Chat).WithMany(p => p.Messages).HasForeignKey(d => d.ChatId);
-
             entity.HasOne(d => d.Sender).WithMany(p => p.Messages).HasForeignKey(d => d.SenderId);
         });
 
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.HasIndex(e => e.Email, "IX_Users_Email").IsUnique();
-
             entity.HasIndex(e => e.PhoneNumber, "IX_Users_PhoneNumber").IsUnique();
-
             entity.HasIndex(e => e.Username, "IX_Users_Username").IsUnique();
 
             entity.Property(e => e.Id).HasColumnType("bigint(20)");
