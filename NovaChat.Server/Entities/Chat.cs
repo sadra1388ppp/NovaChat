69a9d169589476e4cd2e6a3f764d43f9f701852e
sadra <sadrap31@gmail.com>
2026-09-11 18:43:34 +0330

    Refactor Chat entity to use ChatMembers only

--- NovaChat.Server/Entities/Chat.cs
+++ NovaChat.Server/Entities/Chat.cs
@@ -14,10 +14,6 @@ public class Chat
     public string? AvatarUrl { get; set; }
     public long? CreatedByUserId { get; set; }
     public User? CreatedByUser { get; set; }
-    public long? User1Id { get; set; }
-    public User? User1 { get; set; }
-    public long? User2Id { get; set; }
-    public User? User2 { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public ICollection<ChatMember> Members { get; set; } = new List<ChatMember>();
     public ICollection<Message> Messages { get; set; } = new List<Message>();
