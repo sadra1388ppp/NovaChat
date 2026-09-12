2a3713aceb89eb3a3929139a59fcad3eb347f046
sadra <sadrap31@gmail.com>
2026-09-12 09:43:09 +0330

    feat: register message read receipt service

--- NovaChat.Server/Program.cs
+++ NovaChat.Server/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddSingleton<PasswordHashService>();
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<AdminService>();
 builder.Services.AddScoped<ChatService>();
+builder.Services.AddScoped<MessageReadService>();
 builder.Services.AddSingleton<PresenceService>();
 builder.Services.AddSingleton<IAuthorizationHandler, OwnerAuthorizationHandler>();
 builder.Services.AddAuthorization(options => options.AddPolicy("OwnerOnly", policy => { policy.RequireAuthenticatedUser(); policy.AddRequirements(new OwnerRequirement()); }));
