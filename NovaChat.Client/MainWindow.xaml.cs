b93216d9802c6278d92f78c777991da19de5309b
sadra <sadrap31@gmail.com>
2026-09-13 10:22:01 +0330

    fix: complete notification UI dependencies

--- NovaChat.Client/MainWindow.xaml.cs
+++ NovaChat.Client/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Threading;
 using NovaChat.Client.Services;
 using NovaChat.Client.Views;
 
