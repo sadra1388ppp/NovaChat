053ce4c3768efbb2a9705aa694dd837846e4a80a
sadra <sadrap31@gmail.com>
2026-09-13 22:04:05 +0330

    Fix registration privacy selection handling

--- NovaChat.Client/Views/RegisterView.Privacy.cs
+++ NovaChat.Client/Views/RegisterView.Privacy.cs
@@ -1,13 +1,21 @@
 using System;
+using System.Windows.Controls;
 
 namespace NovaChat.Client.Views;
 
 public partial class RegisterView
 {
-    private string GetRegistrationMessagePrivacy() =>
-        string.Equals(RegistrationMessagePrivacyBox?.SelectedItem?.ToString(), "Requests only", StringComparison.OrdinalIgnoreCase)
+    private string GetRegistrationMessagePrivacy()
+    {
+        var selected = RegistrationMessagePrivacyBox?.SelectedItem;
+        var value = selected is ComboBoxItem item
+            ? item.Content?.ToString()
+            : selected?.ToString();
+
+        return string.Equals(value, "Requests only", StringComparison.OrdinalIgnoreCase)
             ? "Requests"
             : "Everybody";
+    }
 
     private bool GetRegistrationAllowGroupAdds() => RegistrationAllowGroupAddsBox?.IsChecked != false;
 }
