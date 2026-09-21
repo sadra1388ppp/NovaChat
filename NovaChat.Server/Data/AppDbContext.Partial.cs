071b9f3cb6fa7ecfc67860fdf7812503203009fd
sadra <sadrap31@gmail.com>
2026-09-21 12:59:48 +0330

    Use first available IDs for EF entities

--- NovaChat.Server/Data/AppDbContext.Partial.cs
+++ NovaChat.Server/Data/AppDbContext.Partial.cs
@@ -7,6 +7,82 @@ public partial class AppDbContext
 {
     public virtual DbSet<AuditLog> AuditLogs { get; set; }
 
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var added = ChangeTracker.Entries()
+            .Where(entry => entry.State == EntityState.Added)
+            .Select(entry => entry.Entity)
+            .ToList();
+
+        var byTable = added
+            .Select(entity => (Entity: entity, Table: entity switch
+            {
+                User => "Users",
+                Chat => "Chats",
+                Contact => "Contacts",
+                Message => "Messages",
+                AuditLog => "AuditLogs",
+                _ => null
+            }))
+            .Where(x => x.Table != null)
+            .GroupBy(x => x.Table!)
+            .OrderBy(x => x.Key, StringComparer.Ordinal)
+            .ToList();
+
+        if (byTable.Count == 0)
+            return await base.SaveChangesAsync(cancellationToken);
+
+        var connection = Database.GetDbConnection();
+        var locks = new List<IAsyncDisposable>();
+        var wasClosed = connection.State != System.Data.ConnectionState.Open;
+
+        try
+        {
+            foreach (var group in byTable)
+                locks.Add(await Services.DatabaseIdAllocator.AcquireTableLockAsync(connection, group.Key, cancellationToken));
+
+            foreach (var group in byTable)
+            {
+                var reservedIds = new HashSet<long>();
+                foreach (var item in group)
+                {
+                    switch (item.Entity)
+                    {
+                        case User user when user.Id == 0:
+                            user.Id = await Services.DatabaseIdAllocator.GetFirstAvailableIdAsync(connection, group.Key, reservedIds, cancellationToken);
+                            break;
+
+                        case Chat chat when chat.Id == 0:
+                            chat.Id = checked((int)await Services.DatabaseIdAllocator.GetFirstAvailableIdAsync(connection, group.Key, reservedIds, cancellationToken));
+                            break;
+
+                        case Contact contact when contact.Id == 0:
+                            contact.Id = checked((int)await Services.DatabaseIdAllocator.GetFirstAvailableIdAsync(connection, group.Key, reservedIds, cancellationToken));
+                            break;
+
+                        case Message message when message.Id == 0:
+                            message.Id = checked((int)await Services.DatabaseIdAllocator.GetFirstAvailableIdAsync(connection, group.Key, reservedIds, cancellationToken));
+                            break;
+
+                        case AuditLog auditLog when auditLog.Id == 0:
+                            auditLog.Id = await Services.DatabaseIdAllocator.GetFirstAvailableIdAsync(connection, group.Key, reservedIds, cancellationToken);
+                            break;
+                    }
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        finally
+        {
+            for (var index = locks.Count - 1; index >= 0; index--)
+                await locks[index].DisposeAsync();
+
+            if (wasClosed && connection.State == System.Data.ConnectionState.Open)
+                await connection.CloseAsync();
+        }
+    }
+
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AuditLog>(entity =>
