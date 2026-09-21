6fb74e603f1de2f7d89f30d6f678c5cc38dafbac
sadra <sadrap31@gmail.com>
2026-09-21 14:02:42 +0330

    Simplify HTTP request logging to store only requests

--- NovaChat.Server/Program.cs
+++ NovaChat.Server/Program.cs
@@ -51,7 +51,6 @@ try
     await using var scope = app.Services.CreateAsyncScope();
     await scope.ServiceProvider.GetRequiredService<DatabaseInitializer>().ValidateSchemaAsync();
     var httpRequestLogDb = scope.ServiceProvider.GetRequiredService<HttpRequestLogDbContext>();
-    await httpRequestLogDb.Database.ExecuteSqlRawAsync("DROP TABLE IF EXISTS AuditLogs;");
     await httpRequestLogDb.Database.EnsureCreatedAsync();
 }
 catch (Exception exception) when (exception is not OperationCanceledException)
