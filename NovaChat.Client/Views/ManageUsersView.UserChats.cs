1e8175f7661fa4ad57f47f16f94902d22473050b
sadra <sadrap31@gmail.com>
2026-09-21 09:33:19 +0330

    Make View User Chats a normal fixed action button

--- NovaChat.Client/Views/ManageUsersView.UserChats.cs
+++ NovaChat.Client/Views/ManageUsersView.UserChats.cs
@@ -1,69 +1,16 @@
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace NovaChat.Client.Views;
 
 public partial class ManageUsersView
 {
-    private Button? _viewChatsButton;
-    private static readonly bool _userChatsButtonRegistered = RegisterUserChatsButton();
+    private void ViewChatsButton_Click(object sender, RoutedEventArgs e) => _ = OpenUserChatsAsync();
 
-    private static bool RegisterUserChatsButton()
+    private async Task OpenUserChatsAsync()
     {
-        EventManager.RegisterClassHandler(
-            typeof(ManageUsersView),
-            FrameworkElement.LoadedEvent,
-            new RoutedEventHandler(OnManageUsersLoadedForChats));
-        return true;
-    }
+        if (_selectedUser == null || _isSaving) return;
 
-    private static void OnManageUsersLoadedForChats(object sender, RoutedEventArgs e)
-    {
-        if (sender is ManageUsersView view)
-            view.EnsureViewChatsButton();
-    }
-
-    private void EnsureViewChatsButton()
-    {
-        if (_viewChatsButton != null || DeleteButton.Parent is not Grid grid)
-            return;
-
-        while (grid.RowDefinitions.Count < 10)
-            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-
-        Grid.SetRow(DeleteButton, 6);
-        Grid.SetRow(ValidationText, 9);
-
-        var infoText = grid.Children
-            .OfType<TextBlock>()
-            .FirstOrDefault(x => Grid.GetRow(x) == 6 && x != ValidationText);
-        if (infoText != null)
-            Grid.SetRow(infoText, 7);
-
-        _viewChatsButton = new Button
-        {
-            Content = "💬  View User Chats",
-            Height = 42,
-            Margin = new Thickness(0, 10, 0, 0),
-            HorizontalContentAlignment = HorizontalAlignment.Center,
-            Style = TryFindResource("SecondaryButtonStyle") as Style,
-            IsEnabled = false
-        };
-        _viewChatsButton.Click += ViewChatsButton_Click;
-        Grid.SetRow(_viewChatsButton, 8);
-        grid.Children.Add(_viewChatsButton);
-
-        UsersList.SelectionChanged += (_, _) => UpdateViewChatsButtonState();
-        UpdateViewChatsButtonState();
-    }
-
-    private async void ViewChatsButton_Click(object sender, RoutedEventArgs e)
-    {
-        if (_selectedUser == null || _viewChatsButton == null)
-            return;
-
-        _viewChatsButton.IsEnabled = false;
+        ViewChatsButton.IsEnabled = false;
         try
         {
             var window = new OwnerUserChatsWindow(_selectedUser.Id, _selectedUser.DisplayName)
@@ -79,8 +26,5 @@ public partial class ManageUsersView
     }
 
     private void UpdateViewChatsButtonState()
-    {
-        if (_viewChatsButton != null)
-            _viewChatsButton.IsEnabled = _selectedUser != null && !_isSaving;
-    }
+        => ViewChatsButton.IsEnabled = _selectedUser != null && !_isSaving;
 }
