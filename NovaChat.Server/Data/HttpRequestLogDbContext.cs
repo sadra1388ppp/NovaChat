ef215ae1f588e0406f8b4ef0105d4957f0d1feec
sadra <sadrap31@gmail.com>
2026-09-21 14:10:54 +0330

    Restore full HTTP request logging

--- NovaChat.Server/Data/HttpRequestLogDbContext.cs
+++ NovaChat.Server/Data/HttpRequestLogDbContext.cs
@@ -16,8 +16,33 @@ public sealed class HttpRequestLogDbContext(DbContextOptions<HttpRequestLogDbCon
         {
             entity.ToTable("HttpRequests");
             entity.HasKey(e => e.Id).HasName("PRIMARY");
+            entity.HasIndex(e => e.RequestId, "IX_HttpRequests_RequestId");
+            entity.HasIndex(e => e.UserId, "IX_HttpRequests_UserId");
+            entity.HasIndex(e => e.StatusCode, "IX_HttpRequests_StatusCode");
+            entity.HasIndex(e => e.StartedAt, "IX_HttpRequests_StartedAt");
+            entity.HasIndex(e => e.Path, "IX_HttpRequests_Path");
+
             entity.Property(e => e.Id).HasColumnType("bigint(20)");
-            entity.Property(e => e.Request).HasColumnType("LONGTEXT").IsRequired();
+            entity.Property(e => e.RequestId).HasMaxLength(100).IsRequired();
+            entity.Property(e => e.Method).HasMaxLength(16).IsRequired();
+            entity.Property(e => e.Scheme).HasMaxLength(16).IsRequired();
+            entity.Property(e => e.Host).HasMaxLength(255).IsRequired();
+            entity.Property(e => e.Path).HasMaxLength(2048).IsRequired();
+            entity.Property(e => e.QueryString).HasMaxLength(4096);
+            entity.Property(e => e.Protocol).HasMaxLength(32).IsRequired();
+            entity.Property(e => e.StatusCode).HasColumnType("int");
+            entity.Property(e => e.IsAuthenticated).HasColumnType("tinyint(1)");
+            entity.Property(e => e.UserId).HasColumnType("bigint(20)");
+            entity.Property(e => e.Username).HasMaxLength(32);
+            entity.Property(e => e.IpAddress).HasMaxLength(45);
+            entity.Property(e => e.UserAgent).HasMaxLength(1024);
+            entity.Property(e => e.RequestContentType).HasMaxLength(255);
+            entity.Property(e => e.ResponseContentType).HasMaxLength(255);
+            entity.Property(e => e.StartedAt).HasColumnType("datetime(6)");
+            entity.Property(e => e.CompletedAt).HasColumnType("datetime(6)");
+            entity.Property(e => e.DurationMs).HasColumnType("bigint");
+            entity.Property(e => e.Succeeded).HasColumnType("tinyint(1)");
+            entity.Property(e => e.ExceptionType).HasMaxLength(512);
         });
     }
 
