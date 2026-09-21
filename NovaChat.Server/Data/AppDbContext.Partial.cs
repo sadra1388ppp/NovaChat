8b4f7e6be5542dea2087e4952de7f8b38c7506a9
sadra <sadrap31@gmail.com>
2026-09-21 13:14:14 +0330

    Move User DeviceId into EF User mapping and remove AuditLog mapping

--- NovaChat.Server/Data/AppDbContext.Partial.cs
+++ NovaChat.Server/Data/AppDbContext.Partial.cs
@@ -6,8 +6,6 @@ namespace NovaChat.Server.Data;
 
 public partial class AppDbContext
 {
-    public virtual DbSet<AuditLog> AuditLogs { get; set; }
-
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         var added = ChangeTracker.Entries()
@@ -22,7 +20,6 @@ public partial class AppDbContext
                 Chat => "Chats",
                 Contact => "Contacts",
                 Message => "Messages",
-                AuditLog => "AuditLogs",
                 _ => null
             }))
             .Where(x => x.Table != null)
@@ -62,9 +59,6 @@ public partial class AppDbContext
                         case Message message when message.Id > 0:
                             reservedIds.Add(message.Id);
                             break;
-                        case AuditLog auditLog when auditLog.Id > 0:
-                            reservedIds.Add(auditLog.Id);
-                            break;
                     }
                 }
 
@@ -88,9 +82,6 @@ public partial class AppDbContext
                             message.Id = checked((int)await DatabaseIdAllocator.GetFirstAvailableIdAsync(connection, group.Key, reservedIds, cancellationToken));
                             break;
 
-                        case AuditLog auditLog when auditLog.Id == 0:
-                            auditLog.Id = await DatabaseIdAllocator.GetFirstAvailableIdAsync(connection, group.Key, reservedIds, cancellationToken);
-                            break;
                     }
                 }
             }
@@ -109,36 +100,11 @@ public partial class AppDbContext
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<AuditLog>(entity =>
-        {
-            entity.ToTable("AuditLogs");
-            entity.HasKey(e => e.Id).HasName("PRIMARY");
-            entity.HasIndex(e => e.UserId, "IX_AuditLogs_UserId");
-            entity.HasIndex(e => e.EventType, "IX_AuditLogs_EventType");
-            entity.HasIndex(e => e.ChatId, "IX_AuditLogs_ChatId");
-            entity.HasIndex(e => e.MessageId, "IX_AuditLogs_MessageId");
-            entity.HasIndex(e => e.CreatedAt, "IX_AuditLogs_CreatedAt");
-
-            entity.Property(e => e.Id).HasColumnType("bigint(20)");
-            entity.Property(e => e.UserId).HasColumnType("bigint(20)");
-            entity.Property(e => e.Username).HasMaxLength(32);
-            entity.Property(e => e.Category).HasMaxLength(32).IsRequired();
-            entity.Property(e => e.EventType).HasMaxLength(64).IsRequired();
-            entity.Property(e => e.TargetType).HasMaxLength(32);
-            entity.Property(e => e.TargetId).HasMaxLength(128);
-            entity.Property(e => e.ChatId).HasColumnType("int(11)");
-            entity.Property(e => e.MessageId).HasColumnType("int(11)");
-            entity.Property(e => e.DeviceId).HasMaxLength(128);
-            entity.Property(e => e.RequestId).HasMaxLength(64);
-            entity.Property(e => e.IpAddress).HasMaxLength(45);
-            entity.Property(e => e.UserAgent).HasMaxLength(512);
-            entity.Property(e => e.Succeeded).HasColumnType("tinyint(1)").HasDefaultValue(true);
-            entity.Property(e => e.Details).HasColumnType("text");
-            entity.Property(e => e.CreatedAt).HasColumnType("datetime(6)");
-        });
-
         modelBuilder.Entity<User>(entity =>
         {
+            entity.Property(e => e.DeviceId)
+                .HasMaxLength(64);
+
             entity.Property(e => e.MessagePrivacy)
                 .HasMaxLength(32)
                 .HasDefaultValue("Everybody");
